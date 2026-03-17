dd25e3e8050578f173f2210275dac6934ace0e9f
Ayishath Jumaila K <jumiabdul@gmail.com>
2026-03-17 12:34:28 +0530

    add loading state

--- Frontend/src/store/slices/transactionSlice.js
+++ Frontend/src/store/slices/transactionSlice.js
@@ -2,7 +2,7 @@ import { createSlice } from "@reduxjs/toolkit"
 
 const transactionSlice = createSlice({
     name: "transactions",
-    initialState: [],
+    initialState: null,
     reducers: {
         setTransactions: (state, action) => {
             return action.payload;
